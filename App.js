665911bdf01db3be465d9050eea0044a90f7ed24
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-01-02 04:15:42 +0545

    icons on bottom tab

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 function DetailsScreen() {
 	return (
 		<View
@@ -111,7 +111,42 @@ export default function App() {
 	return (
 		<NavigationContainer>
 			<StatusBar hidden />
-			<Tab.Navigator>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({
+						focused,
+						color,
+						size,
+					}) => {
+						let iconName;
+
+						if (route.name === 'Home') {
+							iconName = focused
+								? 'home'
+								: 'ios-information-circle-outline';
+						} else if (
+							route.name === 'Settings'
+						) {
+							iconName = focused
+								? 'ios-list-box'
+								: 'ios-list';
+						}
+
+						// You can return any component that you like here!
+						return (
+							<Ionicons
+								name={iconName}
+								size={size}
+								color={color}
+							/>
+						);
+					},
+				})}
+				tabBarOptions={{
+					activeTintColor: 'tomato',
+					inactiveTintColor: 'gray',
+				}}
+			>
 				<Tab.Screen
 					name='Home'
 					component={HomeStackScreen}
