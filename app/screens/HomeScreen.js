801e30d6216f5bf0ef930c2fa589ec885b5c8e24
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-01-02 05:30:29 +0545

    rendering flatlist instead of just single card

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -7,64 +7,82 @@ import {
 	View,
 	StatusBar,
 	Image,
+	FlatList,
 } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import axios from 'axios';
 
 export default function HomeScreen({
 	navigation,
 }) {
 	const [movies, setMovies] = useState([]);
 
-	const fetchMovies = async ()=>{
-		await fetch
-	}
+	const fetchMovies = async () => {
+		try {
+			const response = await axios.get(
+				'https://yts.mx/api/v2/list_movies.json'
+			);
+			setMovies(response.data.data.movies);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	useEffect(() => {
-		fetchMovies()
+		fetchMovies();
 	}, []);
 	return (
-		<View
-			style={{
-				flex: 1,
-				padding: 20,
-			}}
-		>
+		<>
 			<Text
 				style={{
 					color: '#E50914',
-					margin: 5,
+					marginTop: 20,
 					fontSize: 30,
 					marginBottom: 20,
+					marginLeft: 20,
 					fontWeight: 'bold',
 				}}
 			>
 				Movies
 			</Text>
-
-			<View>
-				<TouchableOpacity>
-					<Image
-						source={{
-							uri:
-								'https://img.yts.mx/assets/images/movies/summerland_2020/medium-cover.jpg',
-						}}
+			<FlatList
+				data={movies}
+				horizontal
+				renderItem={() => (
+					<View
 						style={{
-							height: 250,
-							width: 150,
-							borderRadius: 10,
-						}}
-					/>
-
-					<Text
-						style={{
-							color: 'white',
-							fontWeight: 'bold',
-							fontSize: 18,
+							flex: 1,
+							padding: 20,
 						}}
 					>
-						Summerland
-					</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
+						<View>
+							<TouchableOpacity>
+								<Image
+									source={{
+										uri:
+											'https://img.yts.mx/assets/images/movies/summerland_2020/medium-cover.jpg',
+									}}
+									style={{
+										height: 250,
+										width: 150,
+										borderRadius: 10,
+									}}
+								/>
+
+								<Text
+									style={{
+										color: 'white',
+										fontWeight: 'bold',
+										fontSize: 18,
+									}}
+								>
+									Summerland
+								</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+				)}
+			/>
+		</>
 	);
 }
